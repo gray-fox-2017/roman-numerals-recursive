9467c4d12784782e527ea36c3ab41786d328f1c0
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-04-25 14:33:01 +0700

    creating code with recursive is done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,28 @@
 function to_roman(input) {
   // start your code here
-  return to_roman(input); 
+  var  lookup = {
+    M:1000,
+    CM:900,
+    D:500,
+    XC:90,
+    L:50,
+    XL:40,
+    X:10,
+    IX:9,
+    V:5,
+    IV:4,
+    I:1
+  },
+  i = 0;
+  if (input == 0){
+    return '';
+  } else {
+    for (i in lookup){
+      if(input >= lookup[i]){
+        return i + to_roman(input-lookup[i]);
+      }
+    }
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
