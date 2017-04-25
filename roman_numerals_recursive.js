524f132daab76fb94ed29976b28e2840020a0b52
ambodalle <ambodalle.st@gmail.com>
2017-04-25 19:04:31 +0700

    roman numeral recursive done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,22 +1,26 @@
+// 689
 function to_roman(input) {
   // start your code here
+  var decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  var roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
 
-  return to_roman(input);
+  for (var i=0; i<decimal.length; i++) {
+    if (input <1) {
+      return "";
+    }
+    if (input >= decimal[i]) {
+      return roman[i] + to_roman(input - decimal [i])
+    }
+  }
+// return to_roman(input);
 }
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
+console.log('3     | III       | ', to_roman(3))
 console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
-
-var result = '';
-var decimal = [1000,500,100,50,10,5,1];
-var roman = ["M","D","C","L","X","V","I"];
-
-var result ='';
-var decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
-var roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
