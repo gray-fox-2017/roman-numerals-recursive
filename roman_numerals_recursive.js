bc3945fc242f4cb8afe9a65d73aae8a3f92c0d10
Antoni Angga <antoniangga14@gmail.com>
2017-04-25 23:19:40 +0700

    Update Terbaru hanya belum bisa Recursive karena ada masalah di bagian temp nya ikut menjadi kosong

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,64 @@
 function to_roman(input) {
-  // start your code here
-  return to_roman(input); 
+    // start your code here
+    while(input < 3000){
+        if(input >=1000){
+            temp += "M";
+           return to_roman(input-=1000);
+        }
+        else if(input >=900){
+            temp += "CM";
+            return to_roman(input-=900);
+        }
+        else if(input >=500){
+            temp += "D";
+            return to_roman(input-=500,"D");
+        }
+        else if(input >=400){
+            temp += "CD";
+            return to_roman(input -= 400);
+        }
+        else if(input >=100){
+            temp += "C";
+            return to_roman(input -= 100);
+        }
+        else if(input >=90){
+            temp += "XC";
+            return to_roman(input -= 90);
+        }
+        else if(input >=50){
+            temp += "L";
+            return to_roman(input -= 50);
+        }
+        else if(input >=40){
+            temp += "XL";
+            return to_roman(input -= 40);
+        }
+        else if(input >=10){
+            temp += "X";
+            return to_roman(input -= 10);
+        }
+        else if(input >=9){
+            temp += "CM";
+            return to_roman(input -= 9);
+        }
+        else if(input >= 5){
+            temp += "V";
+            return to_roman(input -= 5);
+        }
+        else if(input >= 4){
+            temp += "IV";
+            return to_roman(input -= 4);
+        }
+        else if(input >= 1){
+            temp += "I";
+            return to_roman(input -= 1);
+        }
+        else if(input == 0){
+            input = 0;
+            break;
+        }
+    }
+    return temp;
 }
 
 console.log('My totally sweet testing script for new roman\n')
