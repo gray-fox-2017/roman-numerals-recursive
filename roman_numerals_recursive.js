601c0e7ef2b71906148cd3fb3e6de8795efb192d
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-04-25 21:10:41 +0700

    Roman Numerals Recursive done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,7 +1,18 @@
-function to_roman(input) {
-  // start your code here
-  return to_roman(input); 
+function to_roman(num) {
+  var romanCodes = [
+    ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'], // satuan
+    ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'], // puluhan
+    ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'], // ratusan
+    ['', 'M', 'MM', 'MMM'] // ribuan
+  ]
+  if (!num){
+    return '';
+  } else {
+    return romanCodes[num.toString().length-1][parseInt(num.toString()[0])] +
+      to_roman(parseInt(num.toString().split('').splice(1).join('')));
+  }
 }
+console.log(to_roman(476));
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
