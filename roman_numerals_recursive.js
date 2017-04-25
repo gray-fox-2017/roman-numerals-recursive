0efc249d3fd5564beb804607b9ab2c1a9d123a5e
Antoni Angga <antoniangga14@gmail.com>
2017-04-25 23:31:08 +0700

    sudah ketemu solve nya, dan sudah selesai

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -2,63 +2,50 @@ function to_roman(input) {
     // start your code here
     while(input < 3000){
         if(input >=1000){
-            temp += "M";
-           return to_roman(input-=1000);
+           return "M" + to_roman(input-=1000);
         }
         else if(input >=900){
-            temp += "CM";
-            return to_roman(input-=900);
+            return "CM" + to_roman(input-=900);
         }
         else if(input >=500){
-            temp += "D";
-            return to_roman(input-=500,"D");
+            return "D" + to_roman(input-=500,"D");
         }
         else if(input >=400){
-            temp += "CD";
-            return to_roman(input -= 400);
+            return "CD"+to_roman(input -= 400);
         }
         else if(input >=100){
-            temp += "C";
-            return to_roman(input -= 100);
+            return "C"+to_roman(input -= 100);
         }
         else if(input >=90){
-            temp += "XC";
-            return to_roman(input -= 90);
+            return "XC"+to_roman(input -= 90);
         }
         else if(input >=50){
-            temp += "L";
-            return to_roman(input -= 50);
+            return "L"+to_roman(input -= 50);
         }
         else if(input >=40){
-            temp += "XL";
-            return to_roman(input -= 40);
+            return "XL" + to_roman(input -= 40);
         }
         else if(input >=10){
-            temp += "X";
-            return to_roman(input -= 10);
+            return "X"+to_roman(input -= 10);
         }
         else if(input >=9){
-            temp += "CM";
-            return to_roman(input -= 9);
+            return "IX"+to_roman(input -= 9);
         }
         else if(input >= 5){
-            temp += "V";
-            return to_roman(input -= 5);
+            return "V"+to_roman(input -= 5);
         }
         else if(input >= 4){
-            temp += "IV";
-            return to_roman(input -= 4);
+            return "IV"+to_roman(input -= 4);
         }
         else if(input >= 1){
-            temp += "I";
-            return to_roman(input -= 1);
+            return "I"+to_roman(input -= 1);
         }
         else if(input == 0){
             input = 0;
+            return "";
             break;
         }
     }
-    return temp;
 }
 
 console.log('My totally sweet testing script for new roman\n')
