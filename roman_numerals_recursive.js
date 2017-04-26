4b7bc4cbb9227de4cda227559e0c2e62659e014b
hakiemaul <noersedya@gmail.com>
2017-04-26 17:35:00 +0700

    recursive fixed and finished

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,4 +1,5 @@
 function to_roman(input, str=[]) {
+  // hitung denominator/pengurang/pembagi untuk iterasi ini
   let denominator=function(input) {
     if(input>=1000) return 1000;
     if(input>=500) return 500;
@@ -8,6 +9,7 @@ function to_roman(input, str=[]) {
     if(input>=5) return 5;
     return 1;
   }
+  // cari simbol default untuk iterasi ini
   let simbol=function(input) {
     if(input>=1000) return 'M';
     if(input>=500) return 'D';
@@ -17,18 +19,26 @@ function to_roman(input, str=[]) {
     if(input>=5) return 'V';
     return 'I';
   }
+  // hitung jumlah simbol
   let jumlahSimbol=Math.floor(input/denominator(input));
+  // variabel penghitung
   let hitung=0;
+  let strCek=input-denominator(input);
+  // loop selama masih ada simbol untuk diberikan
   while(jumlahSimbol>=1) {
-    if(input-denominator(input)==4||input-denominator(input)==40||input-denominator(input)==400) {
+    // pemberian simbol untuk angka 9, 19, 90, dll
+    if(strCek==4 && input/denominator(input)>1.5||strCek==40 && input/denominator(input)>1.5||strCek==400 && input/denominator(input)>1.5) {
+      // input/denominator>1.5 untuk mengeluarkan kondisi 14, 140, dll (kondisi ini diselesaikan pada line 42)
       str.push(simbol(input-denominator(input)));
       str.push(simbol(input+denominator(input)));
       jumlahSimbol=0;
       input=denominator(input)*2;
+    // pemberian simbol default
     } else {
       str.push(simbol(input));
       jumlahSimbol--;
       hitung++;
+      // untuk kondisi 4, dll
       if(hitung==4) {
         while(hitung>1) {
           str.pop();
@@ -38,6 +48,7 @@ function to_roman(input, str=[]) {
       }
     }
   }
+  // persiapan untuk iterasi selanjutnya
   input=input%denominator(input);
   if(input==0) {
     return str.join('');
@@ -46,6 +57,10 @@ function to_roman(input, str=[]) {
   }
 }
 
+// for(let i=0;i<=100;i++) {
+//   console.log(to_roman(i))
+// }
+// console.log(to_roman(90))
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|————————')
