f1e7417c108d5ed157c67428dbba5fd6398c7e03
fajarkarim <fajarabdulkarim@gmail.com>
2017-04-28 15:19:35 +0700

    added efficient code

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,34 +1,15 @@
 function to_roman(input) {
+  let arr_numbers = [1000, 900,500,400,100,90,50,40,10,9,5,4,1];
+  let arr_romans = ["M","CM","D","CD","C","CX","L","XL","X","IX","V","IV","I"]
+
   // start your code here
   if (input == 0) {
     return "";
   } else {
-    if (input >= 1000) {
-      return "M" + to_roman(input - 1000);
-    } else if (input >= 900) {
-      return "CM" + to_roman(input - 900);
-    } else if (input >= 500) {
-      return "D" + to_roman(input - 500);
-    } else if (input >= 400) {
-      return "CD" + to_roman(input - 400);
-    } else if (input >= 100) {
-      return "C" + to_roman(input - 100);
-    } else if (input >= 90) {
-      return "XC" + to_roman(input - 90);
-    } else if (input >= 50) {
-      return "L" + to_roman(input - 50);
-    } else if (input >= 40) {
-      return "XL" + to_roman(input - 40);
-    } else if (input >= 10) {
-      return "X" + to_roman(input - 10);
-    } else if (input >= 9) {
-      return "IX" + to_roman(input - 9);
-    } else if (input >= 5) {
-      return "V" + to_roman(input - 5);
-    } else if (input >= 4) {
-      return "IV" + to_roman(input - 4);
-    } else if (input >= 1) {
-      return "I" + to_roman(input - 1);
+    for(let i=0; i<arr_numbers.length;i++){
+      while(input >= arr_numbers[i]){
+          return arr_romans[i] + to_roman(input - arr_numbers[i])
+      }
     }
   }
 }
@@ -41,3 +22,4 @@ console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
+console.log('2519 | MDCXLVI  | ', to_roman(2519))
